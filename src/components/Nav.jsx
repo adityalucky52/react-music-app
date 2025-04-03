b2f2838a3b7d521da00dabcb311a387bfb4be35c
aditya lucky <adityalucky52@gmail.com>
2025-04-03 17:10:22 +0530

    second commit

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -8,18 +8,13 @@ import { Link } from "react-router-dom";
 function Nav() {
    return (
       <div className="w-full h-[100px] bg-black fixed bottom-0 md:top-0 text-white flex justify-around md:justify-center items-center gap-[50px] p-[10px]">
-         <Link to={"/"}>
-            <TiHome className="w-[28px] h-[28px]" />
-         </Link>
-         <Link to={"/search"}>
-            <IoSearch className="w-[28px] h-[28px]" />
-         </Link>
-         <Link to={"/playlist"}>
-            <RiPlayListLine className="w-[228x] h-[28px]" />
-         </Link>
-         <Link to={"/liked"}>
-            <IoMdHeart className="w-[28px] h-[28px]" />
-         </Link>
+         <TiHome className="w-[28px] h-[28px]" />
+
+         <IoSearch className="w-[28px] h-[28px]" />
+
+         <RiPlayListLine className="w-[228x] h-[28px]" />
+
+         <IoMdHeart className="w-[28px] h-[28px]" />
       </div>
    );
 }
